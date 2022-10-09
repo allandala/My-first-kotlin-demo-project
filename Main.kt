3cbe4f993447a18a4c08cc1aebcf2d3c0154d7ac
Mr allan <96319044+allanwise@users.noreply.github.com>
2022-10-09 17:32:20 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+class Main {
+    fun main(args: Array<String>) {
+    //marks scored every term 
+        var comment:String = "Your total marks of the year are"
+        var firstTermMarks = 100
+        var secondTermMarks = 43
+        var thirdTermMarks = 67
+        //total marks
+        var total = firstTermMarks + secondTermMarks + thirdTermMarks
+        // Comments 
+        if (total >= 180){
+       var teacherComnent = "You tried"
+        }
+      println("$comment $total " )
+     
+    }
+}
